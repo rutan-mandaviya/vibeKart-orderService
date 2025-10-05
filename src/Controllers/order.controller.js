dd084a6fac956e5997c4a319e7e89a402f435f0a
rutan-mandaviya <rutanmandaviya14@gmail.com>
2025-10-05 21:06:59 +0530

    solve mismatch erros

--- src/Controllers/order.controller.js
+++ src/Controllers/order.controller.js
@@ -5,7 +5,8 @@ async function createOrder(req,res) {
 
     
 const user=req.user
-const token=req.cookies?.token || req.headers.authorization?.split(' ')[1]
+const token = req.cookies?.token || (req.headers.authorization ? req.headers.authorization.split(' ')[1] : null);
+if(!token) return res.status(401).json({message: "No token provided"});
 
 try {
     const cartResponse=await axios.get('https://vibekart-cartservice.onrender.com/api/cart',{
