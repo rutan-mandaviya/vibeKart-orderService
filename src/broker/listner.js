1c0a6b80e4366244e597f1d359a4a4d3d3692af8
rutan-mandaviya <rutanmandaviya14@gmail.com>
2025-10-06 18:11:46 +0530

    order confirm update

--- src/broker/listner.js
+++ src/broker/listner.js
@@ -6,7 +6,7 @@ const orderModel = require('../model/order.model');
 module.exports = async function (){
 
 
-    subscribeToQueue("order_service_payment_completed", async (data) => {
-        await orderModel.findByIdAndUpdate(data.order, { status: data.status });
-    });
+    // subscribeToQueue("order_service_payment_completed", async (data) => {
+    //     await orderModel.findByIdAndUpdate(data.order, { status: data.status });
+    // });
 }
