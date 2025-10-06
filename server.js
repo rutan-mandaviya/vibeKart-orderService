c483d9847a67aa51254bbd7bb0f888d5d5ac6b76
rutan-mandaviya <rutanmandaviya14@gmail.com>
2025-10-06 17:21:33 +0530

    order confirm update

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ require('dotenv').config()
 const { connect } = require('./src/broker/broker');
 const app=require('./src/app');
 const connectDB = require('./src/db/db');
+const listedner=require('./src/broker/listner')
 
 connectDB()
-connect()
+connect().then(()=>{
+    listedner();
+}).catch((err)=>{
+    console.log("Error connecting to RabbitMQ",err);
+});
 app.listen(3003,()=>{
      
     console.log("server started on port 3003 ");
---
+++ src/broker/listner.js
@@ -0,0 +1,12 @@
+const {subscribeToQueue} = require('./broker');
+// const userModel = require('../models/user.model');
+// const productModel = require('../models/product.model');
+const orderModel = require('../model/order.model');
+// const paymentModel = require('../models/payement.model');
+module.exports = async function (){
+
+
+    subscribeToQueue("order_service_payment_completed", async (data) => {
+        await orderModel.findByIdAndUpdate(data.order, { status: data.status });
+    });
+}
